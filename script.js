3646875779314b46c914f994910784a7df6a4f5a
yousefiraq <50072463+yousefiraq@users.noreply.github.com>
2025-03-13 19:44:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import { db, collection, addDoc } from "./firebase-config.js";
 
-// إدارة الوضع الداكن
 document.getElementById("darkModeToggle").addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
     localStorage.setItem("darkMode", document.body.classList.contains("dark-mode"));
@@ -12,12 +11,10 @@ window.addEventListener('load', () => {
     }
 });
 
-// فتح في خرائط Google
 window.openGoogleMaps = (lat, lng) => {
     window.open(`https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`, '_blank');
 };
 
-// فتح في Waze
 window.openWaze = (lat, lng) => {
     window.open(`https://www.waze.com/ul?ll=${lat},${lng}&navigate=yes`, '_blank');
 };
